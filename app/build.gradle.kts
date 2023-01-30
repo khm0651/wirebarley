a195f1962bc8b3f5270b3a2ad1f13f77e0afd8a3
hare <khm0651@gmail.com>
2023-01-30 17:19:25 +0900

    feat: add exchangeRate View with navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":feature:exchangeRate"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.android.material)
